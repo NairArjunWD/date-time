9aa206f944ef559121018728f23856fa014bd618
Arjun Nair <nair.arjun47@yahoo.com>
2020-11-04 22:29:35 -0800

    Add Central Time Zone
    Add central time zone to be included to make sure it is working.

--- Program.cs
+++ Program.cs
@@ -9,11 +9,15 @@ namespace date_time
             var now = DateTime.Now;
 
             TimeZoneInfo easternTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+            TimeZoneInfo centralTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
 
             var easternTime = TimeZoneInfo.ConvertTime(now, easternTimeZone);
+            var centralTime = TimeZoneInfo.ConvertTime(now, centralTimeZone);
+            
 
             Console.WriteLine(now);
             Console.WriteLine(easternTime);
+            Console.WriteLine(centralTime);
         }
     }
 }
