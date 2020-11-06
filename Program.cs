273227a213c0240228ccec4741565b373dbd11bf
Arjun Nair <nair.arjun47@yahoo.com>
2020-11-05 22:01:25 -0800

    Add New Time Zone
    Add Mountain time. Figure out why other time zones have problems before commiting them.

--- Program.cs
+++ Program.cs
@@ -10,14 +10,17 @@ namespace date_time
 
             TimeZoneInfo easternTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
             TimeZoneInfo centralTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Standard Time");
+            TimeZoneInfo mountainTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Mountain Time");
 
             var easternTime = TimeZoneInfo.ConvertTime(now, easternTimeZone);
             var centralTime = TimeZoneInfo.ConvertTime(now, centralTimeZone);
+            var mountainTime = TimeZoneInfo.ConvertTime(now, mountainTimeZone);
             
 
             Console.WriteLine(now);
             Console.WriteLine(easternTime);
             Console.WriteLine(centralTime);
+            Console.WriteLine(mountainTime);
         }
     }
 }
